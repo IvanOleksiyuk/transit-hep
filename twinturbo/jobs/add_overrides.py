2838d0241d165689cb3cbec2792770f04f906f3c
Ivan Oleksiyuk <ivan.oleksiyuk@gmail.com>
2024-05-30 21:47:30 +0200

    many changes

--- twinturbo/jobs/add_overrides.py
+++ twinturbo/jobs/add_overrides.py
@@ -51,9 +51,9 @@ def modify_slurm_jobs(source_dir, target_dir, line_part_to_find, addition_to_lin
 
 if __name__ == "__main__":
     source_directory = "/home/users/o/oleksiyu/WORK/hyperproject/twinturbo/jobs/experiments_2d_gauss_usem"  # Change this to your source directory
-    target_directory = "/home/users/o/oleksiyu/WORK/hyperproject/twinturbo/jobs/experiments_2d_gauss_nom"  # Change this to your target directory
+    target_directory = "/home/users/o/oleksiyu/WORK/hyperproject/twinturbo/jobs/experiments_4d_gausssph_usem"  # Change this to your target directory
 
     line_part_to_find = "python /home/users/o/oleksiyu/WORK/hyperproject/twinturbo/scripts/full_run.py"
-    addition_to_line = "data=gauss_corr_4_twinturbo_nom"
+    addition_to_line = "data=gauss_sph_4_twinturbo_usem general/subfolder=gauss_sph_4_twinturbo_usem/"
 
     modify_slurm_jobs(source_directory, target_directory, line_part_to_find, addition_to_line)
---
+++ twinturbo/jobs/start_all_experiments.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+
+def submit_slurm_jobs(directory, filename_prefix):
+    # Change the working directory to home
+    os.chdir(os.path.expanduser("~"))
+    
+    for filename in os.listdir(directory):
+        if filename.startswith(filename_prefix):
+            filepath = os.path.join(directory, filename)
+            if os.path.isfile(filepath):
+                print(f"Submitting job: {filepath}")
+                result = subprocess.run(['sbatch', filepath], capture_output=True, text=True)
+                if result.returncode == 0:
+                    print(f"Job submitted successfully: {result.stdout}")
+                else:
+                    print(f"Failed to submit job: {result.stderr}")
+
+if __name__ == "__main__":
+    directory = "/home/users/o/oleksiyu/WORK/hyperproject/twinturbo/jobs/experiments_4d_gauss_usem"  # Change this to your specific folder
+    filename_prefix = "twinturbo"
+
+    submit_slurm_jobs(directory, filename_prefix)

b3c4d3dd20f125f2856d6f0f6474783e2cf479de
Ivan Oleksiyuk <ivan.oleksiyuk@gmail.com>
2024-08-29 20:01:45 +0200

    begfix

--- twinturbo/jobs/experiments29_08_2024/TT_reco_cons0.01_smls0.001_adv3_LCR_2048b_MSlr2-4_to05_dl.sh
+++ twinturbo/jobs/experiments29_08_2024/TT_reco_cons0.01_smls0.001_adv3_LCR_2048b_MSlr2-4_to05_dl.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-#SBATCH --job-name=TT_reco_cons0.01_smls0.001_adv3_LCR_2048b_MSlr2-4_dl
+#SBATCH --job-name=TT_reco_cons0.01_smls0.001_adv3_LCR_2048b_MSlr2-4_to05_dl
 #SBATCH --cpus-per-task=16
 #SBATCH --ntasks=1
 #SBATCH --time=5:00:00
 #SBATCH --partition=shared-gpu,private-dpnc-gpu
 #SBATCH --nodes=1
-#SBATCH --output=/home/users/o/oleksiyu/WORK/hyperproject/twinturbo/jobs/job_output/TT_reco_cons0.01_smls0.001_adv3_LCR_2048b_MSlr2-4_dl-%A-%x_%a.out
+#SBATCH --output=/home/users/o/oleksiyu/WORK/hyperproject/twinturbo/jobs/job_output/TT_reco_cons0.01_smls0.001_adv3_LCR_2048b_MSlr2-4_to05_dl-%A-%x_%a.out
 #SBATCH --mem=16GB
 #SBATCH --gres=gpu:1
 
@@ -18,7 +18,7 @@ cd scratch/sing_images/
 singularity exec --nv -B /home/users/,/srv,/tmp hyperproject_container.sif \
  bash -c "cd /home/users/o/oleksiyu/WORK/hyperproject/ &&\
  python /home/users/o/oleksiyu/WORK/hyperproject/twinturbo/scripts/full_run.py\
- --config-name TT_reco_cons0.01_smls0.001_adv3_LCR_2048b_MSlr2-4_dl general.subfolder=2024_08_29/"
+ --config-name TT_reco_cons0.01_smls0.001_adv3_LCR_2048b_MSlr2-4_to05_dl general.subfolder=2024_08_29/"
 
 # Record the end time
 end_time=$(date +%s)
